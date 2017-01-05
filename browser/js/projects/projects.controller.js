79d32c2a51eafca3d9f6dab8dad1842e6a62552b
Jose <Jrenteria2016@outlook.com>
2017-01-05 10:32:40 -0500

    added video

--- browser/js/projects/projects.controller.js
+++ browser/js/projects/projects.controller.js
@@ -5,15 +5,19 @@ app.controller('projectController', function($scope, allProjects) {
 app.controller('singleProjectController', function($scope, projectFactory, currentProject, $sce) {
     $scope.currentProject = currentProject;
 
-      $scope.trustSrc = function(src) {
-    return $sce.trustAsResourceUrl(src);
-  }
+    $scope.trustSrc = function(src) {
+        return $sce.trustAsResourceUrl(src);
+    }
 
 
 
-  $scope.movie = {src: $scope.currentProject.videoLink}
+    $scope.movie = { src: $scope.currentProject.videoLink }
 
     $scope.hasVideoLink = function hasVideoLink() {
-    	return !!currentProject.videoLink
+        return !!currentProject.videoLink
     }
 })
+
+
+
+
