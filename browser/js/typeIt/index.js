fa1c34559adcbc4905201d5ffb2d2e237d915e32
Jose <Jrenteria2016@outlook.com>
2017-01-18 08:21:32 -0500

    seed changes

--- browser/js/typeIt/index.js
+++ browser/js/typeIt/index.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
       $(".developer")
       .typed({
-        strings: ["Front-end", "Back-end", "Fullstack", "Software","Javascript","Angular"],
+        strings: ["Front-end", "Back-end", "Fullstack", "Software","JavaScript","AngularJS"],
         typeSpeed: 250,
         loop: true,
         cursorSpeed:100,
