4c7121b6a87050b9c2b43785b6c85b0d2f51d8b0
Jose <Jrenteria2016@outlook.com>
2017-01-20 14:45:23 -0500

    added gulp process to bundle and minify js code

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,27 +1,27 @@
-console.log('angular loaded')
-var app = angular.module('myApp', ['ui.router', 'ui.bootstrap', 'ngAnimate', 'xeditable','angular-typed']);
 
-app.run(function(editableOptions) {
-    editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-});
+var app = angular.module('myApp', ['ui.router', 'ui.bootstrap', 'ngAnimate','angular-typed']);
+
+// app.run(function(editableOptions) {
+//     editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+// });
 
 app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
     $locationProvider.html5Mode({
-        // enabled: true,
-        // requireBase: false
+        enabled: true,
+        requireBase: false
     });
     $urlRouterProvider.otherwise("/");
 }]);
 
-app.run(function($rootScope, $state, authFactory) {
-    $rootScope.admin = null;
-    $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams, authFactory) {
-        if (toState.authenticate && !authFactory.isAdmin()) {
-            // User isn’t authenticated
-            $state.transitionTo("home");
-            event.preventDefault();
-        }
-    });
-});
+// app.run(function($rootScope, $state, authFactory) {
+//     $rootScope.admin = null;
+//     $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams, authFactory) {
+//         if (toState.authenticate && !authFactory.isAdmin()) {
+//             // User isn’t authenticated
+//             $state.transitionTo("home");
+//             event.preventDefault();
+//         }
+//     });
+// });
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
-var gulp = require('gulp');
-var minifyCSS = require('gulp-minify-css');
-var minifyHTML = require('gulp-minify-html'); // Minify HTML  
-var concat = require('gulp-concat'); // Join all JS files together to save space  
-var stripDebug = require('gulp-strip-debug'); // Remove debugging stuffs  
-var uglify = require('gulp-uglify'); // Minify JavaScript  
-var imagemin = require('gulp-imagemin'); // Minify images  
-var plumber = require('gulp-plumber');
-var sourcemaps = require('gulp-sourcemaps');
-var babel = require('gulp-babel');
-var ngAnnotate = require('gulp-ng-annotate');
+let gulp = require('gulp');
+let minifyCSS = require('gulp-minify-css');
+let minifyHTML = require('gulp-minify-html'); // Minify HTML  
+let concat = require('gulp-concat'); // Join all JS files together to save space  
+let stripDebug = require('gulp-strip-debug'); // Remove debugging stuffs  
+let uglify = require('gulp-uglify'); // Minify JavaScript  
+let imagemin = require('gulp-imagemin'); // Minify images  
+let plumber = require('gulp-plumber');
+let sourcemaps = require('gulp-sourcemaps');
+let babel = require('gulp-babel');
+let ngAnnotate = require('gulp-ng-annotate');
+let livereload = require('gulp-livereload');
+let eslint = require('gulp-eslint');
+let notify = require('gulp-notify');
+
+
+
 // gulp.task('default', function () {
-// 	console.log('Running gulp')
+//  console.log('Running gulp')
 // })
 
 
 // gulp.task('styles', function() {  
 
-// 	console.log('Running styles')
+//  console.log('Running styles')
 //   gulp.src('./browser/asset/mystyles.css')
 //     // .pipe(sass().on('error', sass.logError))
 //     // .pipe(gulp.dest('./app/css'))
@@ -36,16 +42,37 @@ var ngAnnotate = require('gulp-ng-annotate');
 
 // });
 
+gulp.task('reload', function() {
+    livereload.reload();
+})
+
+gulp.task('reloadCSS', function() {
+    return gulp.src('./public/styles.css').pipe(livereload())
+})
+
+gulp.task('lintJS', function() {
+    return gulp.src(['./browser/js/**/*.js', './server/**/*.js'])
+        .pipe(plumber({
+            errorHandler: notify.onError('Linting FAILED! Check your gulp process.')
+        }))
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+})
+
+
 gulp.task('buildJS', function() {
     return gulp.src(['./browser/js/app.js', './browser/js/**/*.js'])
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(concat('main.js'))
-        // .pipe(babel({
-        //     presets: ['es2015']
-        // }))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./builds/production/js'));
+        .pipe(gulp.dest('./public'));
 });
 
 gulp.task('buildJSProduction', function() {
@@ -59,16 +86,11 @@ gulp.task('buildJSProduction', function() {
         .pipe(ngAnnotate())
         .pipe(uglify())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./builds/production/js'));
+        .pipe(gulp.dest('./public'));
 });
 
 gulp.task('buildCSSProduction', function() {
     return gulp.src('./browser/asset/mystyle.css')
         .pipe(minifyCSS())
         .pipe(gulp.dest('./builds/production/asset'))
-});
-
-gulp.task('copyAssets', function() {
-   gulp.src('./browser/asset/fonts/**/*.{ttf,woff,eof,svg}')
-   .pipe(gulp.dest('./fonts'));
 });
