a63c0f1b2030460c7f9cbc70d0e5c53c3e43cd46
Jose <Jrenteria2016@outlook.com>
2017-01-23 12:07:15 -0500

    added UX/UI to type it list

--- browser/js/typeIt/index.js
+++ browser/js/typeIt/index.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
       $(".developer")
       .typed({
-        strings: ["Front-end", "Back-end", "Fullstack", "Software","JavaScript","AngularJS"],
+        strings: ["Front-end", "Back-end", "Fullstack", "Software","JavaScript","AngularJS","UI/UX"],
         typeSpeed: 250,
         loop: true,
         cursorSpeed:100,
