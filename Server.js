0042583b44415a28197ae5731b9ef93efc3ffe08
Jose Renteria <JoseRenteria.sf@outlook.com>
2018-04-08 21:12:24 -0700

    updated with new project

--- Server.js
+++ Server.js
@@ -1,9 +1,4 @@
-// var connect = require('connect');
-// var serveStatic = require('serve-static');
-// connect().use(serveStatic(__dirname)).listen(8080, function(){
-//     console.log('Server running on 8080...');
-// });
-//
+
 
 
 let express = require('express'),
